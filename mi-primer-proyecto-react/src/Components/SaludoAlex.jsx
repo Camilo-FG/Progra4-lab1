0bb546f4b25510f03bd95fb171913f038ff90ff0
Elian Fajardo Gutierrez <eliancfg.fajardo@gmail.com>
2026-03-19 19:25:13 -0600

    Minor changes

---
+++ mi-primer-proyecto-react/src/Components/SaludoAlex.jsx
@@ -0,0 +1,16 @@
+
+import './SaludoAlex.css'
+
+export default function SaludoAlex() {
+    const name = 'Alex'
+    return (
+        <section className='container'>
+            <section className='saludo'>
+                <h2 className='saludo_title'>!Hola, {name} !</h2>
+                <p className='saludo_extra'>
+                    Aquí hay un mensaje adicional!
+                </p>
+            </section>
+        </section >
+    )
+}
