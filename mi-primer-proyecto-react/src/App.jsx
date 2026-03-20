71cebb30b1e7385f161f40c5db988b43903b7329
Keril-valle <kvalle1306@gmail.com>
2026-03-19 19:35:33 -0600

    primer comit de keril

--- mi-primer-proyecto-react/src/App.jsx
+++ mi-primer-proyecto-react/src/App.jsx
@@ -2,13 +2,13 @@
 
 import './App.css'
 import SaludoAlex from './Components/SaludoAlex'
-
+import SaludoKeril from './Components/SaludoKeril '
 function App() {
 
 
   return (
     <>
-
+       <SaludoKeril />
       <SaludoAlex />
     </>
   )
---
+++ mi-primer-proyecto-react/src/Components/SaludoKeril .jsx
@@ -0,0 +1,17 @@
+
+import { useState } from 'react'
+import './SaludoAlex.css'
+
+export default function SaludoAlex() {
+    const [name, setName] = useState('Keril')
+    return (
+        <section className='container'>
+            <section className='saludo'>
+                <h2 className='saludo_title'>!Hola, {name} !</h2>
+                <p className='saludo_extra'>
+                    Aquí hay un mensaje adicional!
+                </p>
+            </section>
+        </section >
+    )
+}
